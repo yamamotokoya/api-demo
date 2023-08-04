ff8fd5c13f80b05c3da343f7819f31357ee16307
Koya Yamamoto <koyayamamoto0@gmail.com>
2023-08-04 11:26:04 +0900

    add uesrs_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def index 
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save 
+      render json: user 
+    else
+      render json: {status: "error"}
+    end 
+  end
 end
